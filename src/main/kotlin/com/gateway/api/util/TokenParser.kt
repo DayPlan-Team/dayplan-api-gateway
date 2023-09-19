c0e183dfc237bac7272a2370e1a754f40c08adbf
gosekose <88478829+gosekose@users.noreply.github.com>
2023-09-20 00:25:03 +0900

    [Fix] DPLG-006: 토큰 파싱 문제 해결 (#5)

--- src/main/kotlin/com/gateway/api/util/TokenParser.kt
+++ src/main/kotlin/com/gateway/api/util/TokenParser.kt
@@ -4,15 +4,27 @@ import com.gateway.api.exception.exception.GatewayException
 import com.gateway.api.exception.exception.GatewayExceptionCode
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.io.Decoders
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.InitializingBean
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.security.Key
 import java.util.*
 
-object TokenParser {
-
-    private const val TOKEN_PREFIX = "Bearer "
+@Component
+class TokenParser : InitializingBean {
 
     @Value("\${jwt.secret-key}")
     private lateinit var secretKey: String
+
+    private lateinit var key: Key
+
+    override fun afterPropertiesSet() {
+        val keyBytes = Decoders.BASE64.decode(secretKey)
+        key = Keys.hmacShaKeyFor(keyBytes)
+    }
+
 
     fun parseUserIdFromToken(token: String, headerType: HeaderType): String {
         val claims = parseToken(token)
@@ -36,11 +48,16 @@ object TokenParser {
 
     private fun parseToken(token: String): Claims {
         val refinedToken = removeTokenPrefix(token)
+        log.info("refinedToken = $refinedToken")
 
-        return Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(refinedToken).body
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(refinedToken).body
     }
 
     private fun removeTokenPrefix(token: String): String {
         return token.replace(TOKEN_PREFIX, "")
+    }
+
+    companion object : Logger() {
+        private const val TOKEN_PREFIX = "Bearer "
     }
 }
